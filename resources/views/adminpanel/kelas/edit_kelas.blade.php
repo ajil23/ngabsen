370d17bd4bd8c943bf35c032bb3897daf0d0d64e
ajil23 <azrilpraya@gmail.com>
2024-02-24 12:16:23 +0800

    add pagination in kelas table

--- resources/views/adminpanel/kelas/edit_kelas.blade.php
+++ resources/views/adminpanel/kelas/edit_kelas.blade.php
@@ -60,7 +60,7 @@
                     </div>
                 </div>
                 <div class="w-full max-w-full px-3 shrink-0 md:w-full md:flex-0">
-                    <button type="button" onclick="history.back()" class="inline-block px-8 py-2 mb-4 ml-auto font-bold leading-normal text-center text-red-500 align-middle transition-all ease-in bg-transparent border-0 rounded-lg shadow-md cursor-pointer text-xs tracking-tight-rem hover:shadow-xs hover:-translate-y-px active:opacity-85">Batal</button>
+                   <button type="button" onclick="history.back()" class="inline-block px-8 py-2 mb-4 mr-3 font-bold text-center text-blue-500 align-middle transition-all bg-transparent border border-blue-500 rounded-lg cursor-pointer leading-normal text-xs ease-in tracking-tight-rem shadow-xs bg-150 bg-x-25 hover:-translate-y-px active:opacity-85 hover:shadow-md">Batal</button>
                     <button type="submit" class="inline-block px-8 py-2 mb-4 ml-auto font-bold leading-normal text-center text-white align-middle transition-all ease-in bg-blue-500 border-0 rounded-lg shadow-md cursor-pointer text-xs tracking-tight-rem hover:shadow-xs hover:-translate-y-px active:opacity-85">Simpan</button>
                 </div>
             </form>
