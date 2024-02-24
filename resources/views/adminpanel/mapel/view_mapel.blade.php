b1a3c0f2451069fbc920e80a60e61ab8b88a7e68
ajil23 <azrilpraya@gmail.com>
2024-02-24 18:33:32 +0800

    search bar for kelas table

--- resources/views/adminpanel/mapel/view_mapel.blade.php
+++ resources/views/adminpanel/mapel/view_mapel.blade.php
@@ -2,11 +2,17 @@
 @section('admin')
 <nav class="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all ease-in shadow-none duration-250 rounded-2xl lg:flex-nowrap lg:justify-start" navbar-main navbar-scroll="false">
     <div class="flex items-center justify-between w-full px-4 py-1 mx-auto flex-wrap-inherit">
-  
+        <h6 class="mb-0 font-bold text-blue-500 capitalize">Mata Pelajaran</h6>
       <div class="flex items-center mt-2 grow sm:mt-0 sm:mr-6 md:mr-0 lg:flex lg:basis-auto">
-        <h6 class="mb-0 font-bold text-blue-500 capitalize">Mapel</h6>
+        
         <div class="flex items-center md:ml-auto md:pr-4">
-         
+          <div class="flex items-center md:ml-auto md:pr-4">
+            <div class="relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease">
+              <span class="text-sm ease leading-5.6 absolute z-50 -ml-px flex h-full items-center whitespace-nowrap rounded-lg rounded-tr-none rounded-br-none border border-r-0 border-transparent bg-transparent py-2 px-2.5 text-center font-normal text-slate-500 transition-all">
+                <i class="fas fa-search"></i>
+              </span>
+              <input type="text" class="pl-9 text-sm focus:shadow-primary-outline ease w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 dark:bg-slate-850 dark:text-white bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none focus:transition-shadow" placeholder="Cari..." />
+            </div>
         </div>
         <ul class="flex flex-row justify-end pl-0 mb-0 list-none md-max:w-full">
           <!-- online builder btn  -->
@@ -14,7 +20,7 @@
             <a class="inline-block px-8 py-2 mb-0 mr-4 text-xs font-bold text-center text-blue-500 uppercase align-middle transition-all ease-in bg-transparent border border-blue-500 border-solid rounded-lg shadow-none cursor-pointer leading-pro hover:-translate-y-px active:shadow-xs hover:border-blue-500 active:bg-blue-500 active:hover:text-blue-500 hover:text-blue-500 tracking-tight-rem hover:bg-transparent hover:opacity-75 hover:shadow-none active:text-white active:hover:bg-transparent" target="_blank" href="https://www.creative-tim.com/builder/soft-ui?ref=navbar-dashboard&amp;_ga=2.76518741.1192788655.1647724933-1242940210.1644448053">Online Builder</a>
           </li> -->
           <li class="flex items-center">
-            <a href="./pages/sign-in.html" class="block px-0 py-2 text-sm font-semibold text-blue-500 transition-all ease-nav-brand">
+            <a href="#" class="block px-0 py-2 text-sm font-semibold text-blue-500 transition-all ease-nav-brand">
               <i class="fa fa-user sm:mr-1"></i>
               <span class="hidden sm:inline">{{Auth::user()->name}}</span>
             </a>
@@ -34,9 +40,62 @@
   </nav>
 
   <div class="w-full px-6 py-6 mx-auto">
-    <!-- row 1 -->
+    <!-- table 1 -->
     <div class="flex flex-wrap -mx-3">
-     <h1>Ini Adalah Halaman Mapel</h1>
+      <div class="flex-none w-full max-w-full px-3">
+        <div class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
+          <div class="p-6 pb-0 mb-0 border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
+            <h6 class="dark:text-white">Tabel Mata Pelajaran</h6>
+          </div>
+          <div class="flex-auto px-0 pt-0 pb-2">
+            <div class="p-0 overflow-x-auto">
+              <table class="items-center w-full mb-0 align-top border-collapse dark:border-white/40 text-slate-500">
+                <thead class="align-bottom">
+                  <tr>
+                    <th class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-collapse shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">No</th>
+                    <th class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-collapse shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">Nama Kelas</th>
+                    <th class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-collapse shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">Jumlah Siswa</th>
+                    <th class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-collapse shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">Angkatan</th>
+                    <th class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-collapse shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">Aksi</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {{-- @foreach ($datakelas as $item) --}}
+                  <tr>
+                    <td class="p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
+                      <span class="text-xs font-semibold leading-tight dark:text-black dark:opacity-80 text-black">1</span>
+                    </td>
+                    <td class="p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
+                      <span class="text-xs font-semibold leading-tight dark:text-black dark:opacity-80 text-black"><b>IPA</b></span>
+                    </td>
+                    <td class="p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
+                      <span class="text-xs font-semibold leading-tight dark:text-black dark:opacity-80 text-black">12</span>
+                    </td>
+                    <td class="p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
+                      <span class="text-xs font-semibold leading-tight dark:text-black dark:opacity-80 text-black">21</span>
+                    </td>
+                    <td colspan="2" class="p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
+                      <div class="ml-auto">
+                        <a onclick="confirmation(event)" class="relative z-10 inline-block px-4 py-2.5 mb-0 font-bold text-center text-transparent align-middle transition-all border-0 rounded-lg shadow-none cursor-pointer leading-normal text-sm ease-in bg-150 bg-gradient-to-tl from-red-600 to-orange-600 hover:-translate-y-px active:opacity-85 bg-x-25 bg-clip-text" href="#"><i class="mr-2 far fa-trash-alt bg-150 bg-gradient-to-tl from-red-600 to-orange-600 bg-x-25 bg-clip-text"></i>Delete</a>
+                        <a class="inline-block dark:text-white px-4 py-2.5 mb-0 font-bold text-center align-middle transition-all bg-transparent border-0 rounded-lg shadow-none cursor-pointer leading-normal text-sm ease-in bg-150 hover:-translate-y-px active:opacity-85 bg-x-25 text-slate-700" href="#"><i class="mr-2 fas fa-pencil-alt text-slate-700" aria-hidden="true"></i>Edit</a>
+                      </div>
+                    </td>
+                  </tr>
+                  {{-- @endforeach --}}
+                </tbody>
+              </table>
+              <div class="justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+                {{-- {{$datakelas->links()}} --}}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div fixed-plugin>
+      <a href="#" fixed-plugin-button class="fixed px-4 py-2 text-xl bg-blue-500 shadow-lg cursor-pointer bottom-8 right-8 z-990 rounded-circle text-white">
+        <i class="py-2 pointer-events-none fa fa-solid fa-plus"> </i>
+      </a>
     </div>
   
     @include('admin.components.footer')
